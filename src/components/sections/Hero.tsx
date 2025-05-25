1ac3a71d8ddb11ce138d360a6ed34a3072c5c2c7
Harsh <harsh@growthz.ai>
2025-05-25 23:40:38 +0530

    Updated Content

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -21,15 +21,15 @@ const Hero = forwardRef<HTMLDivElement>((props, ref) => {
       
       {/* Main hero content */}
       <main className="z-10 flex flex-col items-center justify-center text-center px-4 h-screen">
-        <h1 className="text-6xl sm:text-7xl font-bold text-white mb-12 drop-shadow-[0_0_60px_rgba(168,85,247,0.8)] mx-44">
-          Powering Global Payouts and Card Issuing, Instantly.
+        <h1 className="text-6xl sm:text-7xl font-bold text-white mb-10 drop-shadow-[0_0_60px_rgba(168,85,247,0.8)] mx-44">
+        The Platform That Powers Global Payouts & Card Issuing. Instantly.
         </h1>
         <h2 className="text-2xl sm:text-3xl text-white mb-2 font-semibold">
           Instant Payments. Zero Hassle.
         </h2>
         <p className="text-lg sm:text-xl text-zinc-200 mb-8 leading-snug">
-          <span className="whitespace-nowrap">The CleverCards Platform lets you issue digital Mastercards, create accounts, and move funds</span><br />
-          <span className="whitespace-nowrap">instantly, anywhere in the world..</span>
+          <span className="whitespace-nowrap">Issue branded digital Mastercards instantly. Configure spend, manage disbursements, and </span><br />
+          <span className="whitespace-nowrap">reach recipients anywhere, anytime- with no banking details delays.</span>
         </p>
         <div className="flex gap-4 mb-12 flex-col sm:flex-row items-center justify-center">
           <CustomButton size="lg">For Businesses</CustomButton>
